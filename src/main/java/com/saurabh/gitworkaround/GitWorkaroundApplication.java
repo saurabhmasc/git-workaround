a5326e17fc10a635f96f0141a3ac7148d514bdd9
saurabhmasc <saurabhmasc@gmail.com>
2023-04-18 12:12:59 +0530

    Initial commit

---
+++ src/main/java/com/saurabh/gitworkaround/GitWorkaroundApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.gitworkaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWorkaroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWorkaroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/gitworkaround/GitWorkaroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.gitworkaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorkaroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
